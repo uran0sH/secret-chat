3c80c62311bc4a084e17d0ff5e859a328e3118da
RobotHuang <1183598761@qq.com>
2020-03-12 19:04:14 +0800

    init

---
+++ src/main/java/com/hwy/secretchat/SecretChatApplication.java
@@ -0,0 +1,13 @@
+package com.hwy.secretchat;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SecretChatApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SecretChatApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/hwy/secretchat/SecretChatApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hwy.secretchat;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SecretChatApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
